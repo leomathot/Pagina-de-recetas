f4d861bf03ee5e3b87dc24f517e1c69321abd0a7
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-05-10 00:00:18 -0300

    Add JS files, recipe page 01, change colors, etc

--- js/index.js
+++ js/index.js
@@ -1,54 +1,65 @@
+let navNoIndex = `
+        <div class="nav-enlaces">
+            <a href="../index.html">Inicio</a>
+        </div>`;
+
+let navNoIndexTag = document.querySelector("#nav-no-index");
+
+if (navNoIndexTag != null) {
+    navNoIndexTag.innerHTML = navNoIndex;
+}
+
 let footer = `<div class="footer-pagina">
 
-<div class="footer-texto">
-  <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, ab
-    reprehenderit beatae veniam eveniet blanditiis.
-  </p>
-</div>
+    <div class="footer-texto">
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, ab
+        reprehenderit beatae veniam eveniet blanditiis.
+    </p>
+    </div>
 
-<div class="footer-enlaces">
-  <a href="https://www.instagram.com" target="_blank" title="Instagram">
-    <i class="fa-brands fa-instagram"></i >
-  </a>
-  <a href="https://www.facebook.com" target="_blank" title="Facebook">
-    <i class="fa-brands fa-facebook"></i >
-  </a>
-  <a href="https://www.twitter.com" target="_blank" title="Twitter">
-    <i class="fa-brands fa-twitter"></i >
-  </a>
-  <a href="https://www.YouTube.com" target="_blank" title="YouTube">
-    <i class="fa-brands fa-youtube"></i >
-  </a>
-</div>
-</div>
+    <div class="footer-enlaces">
+    <a href="https://www.instagram.com" target="_blank" title="Instagram">
+        <i class="fa-brands fa-instagram"></i >
+    </a>
+    <a href="https://www.facebook.com" target="_blank" title="Facebook">
+        <i class="fa-brands fa-facebook"></i >
+    </a>
+    <a href="https://www.twitter.com" target="_blank" title="Twitter">
+        <i class="fa-brands fa-twitter"></i >
+    </a>
+    <a href="https://www.YouTube.com" target="_blank" title="YouTube">
+        <i class="fa-brands fa-youtube"></i >
+    </a>
+    </div>
+    </div>
 
-<div class="footer-grupo-2">
+    <div class="footer-grupo-2">
 
-<div>
-  <p>Codo a Codo</p>
-  <p>FULL STACK PYTHON</p>
-  <p>2023 - 1er cuat.</p>
-  <p>Comisión 23037</p>
-</div>
+    <div>
+    <p>Codo a Codo</p>
+    <p>FULL STACK PYTHON</p>
+    <p>2023 - 1er cuat.</p>
+    <p>Comisión 23037</p>
+    </div>
 
-<div>
-  <p><strong>Grupo 2</strong></p>
-  <ul>
-    <li>Jesica Moure</li>
-    <li>Malena Sencio</li>
-    <li>Manuel Pérez</li>
-    <li>Leonardo Mathot</li>
-  </ul>
-</div>
+    <div>
+    <p><strong>Grupo 2</strong></p>
+    <ul>
+        <li>Jesica Moure</li>
+        <li>Malena Sencio</li>
+        <li>Manuel Pérez</li>
+        <li>Leonardo Mathot</li>
+    </ul>
+    </div>
 
-<div>
-  <p>Repositorio del proyecto</p>
-  <a href="https://github.com/manuelpdbrc/codoacodo" target="_blank" title="GitHub">
-    <i class="fa-brands fa-github"></i>
-  </a>
-</div>
+    <div>
+    <p>Repositorio del proyecto</p>
+    <a href="https://github.com/manuelpdbrc/codoacodo" target="_blank" title="GitHub">
+        <i class="fa-brands fa-github"></i>
+    </a>
+    </div>
 
-</div>`;
+    </div>`;
 
 document.querySelector("#footer").innerHTML = footer;
---
+++ js/recetas.js
@@ -0,0 +1,75 @@
+const recetas = [
+    {
+        nombre: "Empanadas",
+        descripcion: "Un clásico, en versiones omnívora y veggie. Para preparar mil y congelar!",
+        rutaPagina: "./recetas/receta-01.html",
+        ingredientes: [
+            "12 tapas",
+            "500 gr Cebolla",
+            "500 gr Carne picada o Soja texturizada hidratada",
+            "1 ají rojo o verde",
+            "Comino C/N",
+            "Pimentón dulce C/N",
+            "Sal y pimienta C/N"
+        ],
+        pasos: [
+            "Rehogar la cebolla con el ají",
+            "Cuando la cebolla se transparente, agregar la carne o soja",
+            "Esperar a que la carne cambie toda de color y condimentar",
+            "Dejar enfriar el relleno",
+            "Rellenar las tapas y hacer el repulgue",
+            "Mandar a horno fuerte 10-15 minutos o hasta que se doren"
+        ],
+        rutasFotos: [
+            "./imagenes/empanadas-01.jpg",
+            "./imagenes/empanadas-02.jpg",
+            "./imagenes/empanadas-03.jpg"
+        ]
+    },
+    {
+        nombre: "Milanesas",
+        descripcion: "Sequitas y crocantes. Para disfrutar al plato o en un sánguche completo.",
+        rutaPagina: "./recetas/receta-02.html",
+        ingredientes: [
+            "500 gr Nalga cortada finamente",
+            "25 gr Provenzal",
+            "Una cucharada de mostaza",
+            "3 huevos",
+            "Pan rallado C/N",
+            "Aceite C/N",
+            "Sal y pimienta C/N"
+        ],
+        pasos: [
+            "Limpiar la carne y sacarle el exceso de grasa",
+            "Marinarla con el huevo, sal, pimienta, mostaza y provenzal",
+            "Ir pasándola por pan rallado, presionando bien para que se pegue",
+            "Mandar un rato a la heladera con separadores",
+            "Freir o cocinar al horno con un poco de aceite"
+        ],
+        rutasFotos: [
+            "./imagenes/milas-01.jpg",
+            "./imagenes/milas-02.jpg",
+            "./imagenes/milas-03.jpg"
+        ]
+    }
+]
+
+
+let cards = "";
+for (var i = 0; i < recetas.length; i++) {
+    cards += `
+        <div class="card-receta">
+            <a href=${recetas[i].rutaPagina}>
+                <img
+                    src=${recetas[i].rutasFotos[0]}
+                    alt="describir la foro acá"
+                />
+                <div class="contenedor-texto">
+                    <h4>${recetas[i].nombre}</h4>
+                    <p>${recetas[i].descripcion}</p>
+                </div>
+            </a>
+        </div>`
+}
+
+document.querySelector("#cardsRecetas").innerHTML = cards;
