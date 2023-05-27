7bdae29d835e7d916d135533003e4fe53edaef99
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-05-27 02:19:18 -0300

    Add info and images to the selection recipes

--- js/recipes.js
+++ js/recipes.js
@@ -4,155 +4,173 @@
 const selection = [
     {
         name: "Empanadas",
-        description: "Un clásico, en versiones omnívora y veggie. Para preparar mil y congelar!",
+        description: "A South American cuisine classic. You can bake or fry them.",
         ingredients: [
-            "12 tapas",
-            "500 gr Cebolla",
-            "500 gr Carne picada o Soja texturizada hidratada",
-            "1 ají rojo o verde",
-            "Comino C/N",
-            "Pimentón dulce C/N",
-            "Sal y pimienta C/N"
+            "1.8 kg chopped onion",
+            "300 g of chopped green onion",
+            "300 g melted butter",
+            "1.5 kg minced meat",
+            "12 hard-boiled eggs",
+            "1 teaspoon of ground chili",
+            "1 teaspoon white pepper",
+            "30 g of fine salt",
+            "1 pinch of cumin",
+            "1 teaspoon oregano",
+            "2 tablespoons paprika",
+            "25g of salt",
+            "1 kg of flour",
+            "200 g of beef fat",
+            "melted butter for painting"
         ],
         directions: [
-            "Rehogar la cebolla con el ají",
-            "Cuando la cebolla se transparente, agregar la carne o soja",
-            "Esperar a que la carne cambie toda de color y condimentar",
-            "Dejar enfriar el relleno",
-            "Rellenar las tapas y hacer el repulgue",
-            "Mandar a horno fuerte 10-15 minutos o hasta que se doren"
+            "1. The filling: Transparente the red onions in a covered pot. Cook the minced meat in the butter. When it changes color, add the cooked onion, the green onion and the seasonings, except the paprika, which will be added at the end of cooking.",
+            "2. Cool and place in the fridge until it is time to assemble the empanadas, but keep stirring so that the fat is well distributed throughout the filling.",
+            "3. The dough: Dissolve the salt in the water, gradually add the flour and the melted fat, knead well. There should be a hard dough. Put in the fridge and let rest for an hour.",
+            "4. Roll out the dough 4 mm thick, trying to give it a rectangular shape. Fold it in half and take it back to the fridge. Let it rest for 1 hour. Repeat this step 5 times, letting it rest between each step.",
+            "5. Roll out the dough once more and brush it with melted butter. Sprinkle with cornstarch, fold again and stretch it to a thickness of 2 mm. Cut discs 13 centimeters in diameter to fill.",
+            "6. Place 1 tablespoon of the filling with a little piece of egg in the center of the dough and wet the edge with water. Close and join the edges making a \"repulgue\" (twisting) or pressing with a fork.",
+            "7. Heat the oven (if it is clay one, better) and put the empanadas. In a common, preheated oven, maximum, they take about 10 minutes. (You can also fry them.)"
         ],
         photoPaths: [
-            "./images/empanadas-01.jpg",
-            "./images/empanadas-02.jpg",
-            "./images/empanadas-03.jpg"
+            "/images/empanadas-01.jpg",
+            "/images/empanadas-02.jpg",
+            "/images/empanadas-03.jpg"
+        ]
+    },
+    {
+        name: "Stir-Fried Rice Noodles",
+        description: "This flavorful stir-fry relies on pantry ingredients for a quick and easy dinner.",
+        ingredients: [
+            "8 ounces (227g) dry rice noodles",
+            "4 eggs",
+            "1/2 teaspoon Diamond Crystal kosher salt; for table salt use half as much by volume",
+            "4 tablespoons (60ml) vegetable or canola oil, divided",
+            "2 small heads bok choy (about 4 1/2 ounces; 154g) or other hearty greens, cut into 1/2-inch slices (about 2 cups loosely packed greens)",
+            "2 medium cloves garlic, minced",
+            "2 scallions (1 ounce; 30g), trimmed and thinly sliced crosswise (about 1/3 cup)",
+            "1 teaspoon chile flakes or chile sauce, plus more for serving",
+            "2 to 3 teaspoons light soy sauce (5 to 15ml)"
+        ],
+        directions: [
+            "1. Cook noodles according to package instructions. Rinse cooked noodles under cold running water. Drain well and set aside.",
+            "2. In a small bowl, beat eggs with salt until homogenous. In a wok, heat 2 tablespoons (30ml) oil over high heat until shimmering. Add eggs and cook, scrambling and stir-frying with a spatula, until almost cooked through, about 30 seconds. Transfer to a plate and set aside.",
+            "3. Wipe out wok and add 1 tablespoon (15ml) oil. Heat over high heat until smoking. Add greens and stir-fry until just tender, 1 to 3 minutes. Transfer to a plate and set aside.",
+            "4. Add remaining 1 tablespoon (15ml) oil to wok and heat over high heat until smoking. Add garlic and scallions and cook, stirring constantly, until lightly browned, about 30 seconds. Add chile flakes or chile sauce to wok and stir-fry until fragrant, a few seconds. Add noodles to wok and stir around with a spatula, adding soy sauce as you mix. Stir-fry until noodles are dry and evenly seasoned, 2 to 3 minutes, then add eggs and vegetables and stir around until everything is blended. Serve immediately, with more chile sauce and soy sauce on the side."
+        ],
+        photoPaths: [
+            "/images/rice-noodles-01.jpg",
+            "/images/rice-noodles-02.jpg",
+            "/images/rice-noodles-03.jpg"
+        ]
+    },
+    {
+        name: "Buttermilk & Leek Mashed Potatoes",
+        description: "The world's easiest mashed potatoes.",
+        ingredients: [
+            "2 large leeks, white and light green parts only, thoroughly rinsed and sliced into thin rings",
+            "3 cloves of garlic, minced",
+            "1 tablespoon unsalted butter, plus 4 tablespoons, plus more for garnish (optional, but is it really?)",
+            "1/4 cup dry white wine",
+            "3 pounds Yukon Gold potatoes, scrubbed and cut into six pieces each (I leave mine unpeeled, but you can peel depending on your personal preference)",
+            "3/4 cup chicken broth (you can substitute vegetable)",
+            "1/2 cup buttermilk",
+            "1/2 cup heavy cream",
+            "2 teaspoons (heaping) kosher salt, plus more to taste",
+            "1/2 teaspoon freshly cracked black pepper",
+            "1 handful finely chopped chives or scallions for garnish (optional)"
+        ],
+        directions: [
+            "1. Set your multi-cooker to the sauté setting. Once it\'s hot, cook the garlic and leeks in 1 tablespoon of butter until they begin to get translucent, about 3 minutes. Deglaze with the wine, and bring to a simmer. Add the potatoes and broth and give everything a stir. Cover and set to pressure cook on high for 9 minutes.",
+            "2. Once the pressure cooking is complete, let the pressure release naturally for a few minutes before utilizing the instant release function.",
+            "3. When the pressure is released, uncover and add the remaining 4 tablespoons of butter. Begin to mash the potatoes in the pot. As you do, drizzle in the buttermilk and cream, then mix in the salt and pepper. Mash to your desired smoothness. Adjust seasoning to taste. To serve, garnish with more butter and chopped herbs if using."
+        ],
+        photoPaths: [
+            "/images/mashed-potatoes-01.jpg",
+            "/images/mashed-potatoes-02.jpg",
+            "/images/mashed-potatoes-03.jpg"
+        ]
+    },
+    {
+        name: "Breakfast Burrito",
+        description: "The makings of a great bacon, egg, and cheese breakfast burrito.",
+        ingredients: [
+            "4 large eggs",
+            "Kosher salt",
+            "1/4 cup (60ml) canola, vegetable, or other neutral oil",
+            "1 pound (450g) Russet potatoes (about 2 medium potatoes), peeled and cut into 1/2-inch dice",
+            "1/2 medium (8-ounce; 225g) yellow onion, cut into 1/2-inch dice",
+            "Freshly ground black pepper",
+            "1 tablespoon (15g) unsalted butter",
+            "4 rashers crisply cooked bacon, broken into pieces",
+            "2 ounces (55g) shredded cheddar cheese",
+            "2 large (10-inch) flour tortillas, warmed through on a dry griddle or cast iron pan",
+            "2 tablespoons (1 ounce; 30g) sour cream, divided",
+            "1/4 cup (2 ounces; 55g) guacamole, divided",
+            "Hot sauce of your choice (optional)"
+        ],
+        directions: [
+            "1. In a medium bowl, whisk eggs with a large pinch of salt until homogenous and no visible egg whites remain. Set aside.",
+            "2. In a medium cast iron or stainless steel skillet, heat oil over medium-high heat until shimmering. Add potatoes, season with salt, and cook, stirring only occasionally, until beginning to brown, about 8 minutes. Lower heat to medium, stir in onion, and continue to cook, stirring occasionally to prevent scorching, until potatoes are golden and crisp and onions are browned and tender, about 10 minutes longer. Season with additional salt and pepper to taste and set aside.",
+            "3. In a medium nonstick skillet, melt butter over medium heat until foaming. Add eggs and cook, stirring constantly with a rubber spatula, until eggs have formed curds but are still runny, 1 to 2 minutes. Stir in cheese and continue to cook, stirring constantly, until cheese is melted and eggs are soft and fluffy, about 1 minute longer. Remove from heat.",
+            "4. Working with one just-warmed tortilla at a time, lay tortilla on a work surface. Spread 1 tablespoon (15g) sour cream in a thin, even layer on center of tortilla, leaving about 2 inches of border on either side and 3 inches of border above and below. Top with 2 tablespoons (30g) guacamole and spread it around on top of the sour cream. Arrange half the bacon on top of the guacamole.",
+            "5. Pile half the potatoes on top of the bacon, followed by half the eggs and cheese. Douse with as much hot sauce as you want.",
+            "6. Fold the tortilla sides in over the filling. Then roll the bottom flap of tortilla up over the filling, holding the sides tight as you roll. Continue rolling until the burrito is sealed. Repeat with remaining tortilla and filling ingredients. Serve right away."
+        ],
+        photoPaths: [
+            "/images/burrito-01.jpg",
+            "/images/burrito-02.jpg",
+            "/images/burrito-03.jpg"
+        ]
+    },
+    {
+        name: "Hearty Soup",
+        description: "Enjoy this roasted pepper, sweet potato and paprika soup as a starter, or for lunch.",
+        ingredients: [
+            "1 sweet potato, roughly chopped into dice with the skin still on",
+            "1 red pepper, de-seeded and cut into chunks",
+            "1 red onion, peeled and cut into chunks",
+            "3 garlic cloves, peeled",
+            "1 tsp smoked paprika",
+            "2 tbsp olive oil",
+            "200ml coconut milk",
+            "200ml chicken stock",
+            "½ tbsp sriracha",
+            "1 tsp maple syrup"
+        ],
+        directions: [
+            "1. Heat oven to 190C/170C fan/gas 5. Put the sweet potato, pepper, onion and garlic on a baking tray. Sprinkle with the paprika and seasoning, then drizzle with the oil. Toss together. Roast for 30 mins or until beginning to brown.",
+            "2. Tip the roasted vegetables into a blender (or use a stick blender) with the coconut milk, stock, sriracha and maple syrup. Whizz until smooth. Pour back into the pan and heat until piping hot. Check for seasoning, and pour into a flask. Serve with soda bread or toasted sourdough."
+        ],
+        photoPaths: [
+            "/images/soup-01.jpg",
+            "/images/soup-02.jpg",
+            "/images/soup-03.jpg"
         ]
     },
     {
         name: "Milanesas",
-        description: "Sequitas y crocantes. Para disfrutar al plato o en un sánguche completo.",
+        description: "Super easy and crunchy. To enjoy on the plate or in a complete sandwich.",
         ingredients: [
-            "500 gr Nalga cortada finamente",
-            "25 gr Provenzal",
-            "Una cucharada de mostaza",
-            "3 huevos",
-            "Pan rallado C/N",
-            "Aceite C/N",
-            "Sal y pimienta C/N"
+            "500 gr round steak finely cut (or chicken)",
+             "25 gr provencal",
+             "1 tablespoon mustard",
+             "3 eggs",
+             "Bread crumbs",
+             "Oil",
+             "Salt and pepper"
         ],
         directions: [
-            "Limpiar la carne y sacarle el exceso de grasa",
-            "Marinarla con el huevo, sal, pimienta, mostaza y provenzal",
-            "Ir pasándola por pan rallado, presionando bien para que se pegue",
-            "Mandar un rato a la heladera con separadores",
-            "Freir o cocinar al horno con un poco de aceite"
+            "1. Clean the meat and remove excess fat",
+            "2. Marinate it with the egg, salt, pepper, mustard and Provencal",
+            "3. Pass it through breadcrumbs, pressing well so that it sticks",
+            "4. Send a while to the fridge with separators",
+            "5. Fry or bake with a little oil",
+            "6. Serve with salad, mashed potatoes, French fries, or in a sandwich with sliced tomato, lettuce, sliced ham and cheese, or fried eggs"
         ],
         photoPaths: [
-            "./images/milas-01.jpg",
-            "./images/milas-02.jpg",
-            "./images/milas-03.jpg"
-        ]
-    },
-    {
-        name: "Empanadas",
-        description: "Un clásico, en versiones omnívora y veggie. Para preparar mil y congelar!",
-        ingredients: [
-            "12 tapas",
-            "500 gr Cebolla",
-            "500 gr Carne picada o Soja texturizada hidratada",
-            "1 ají rojo o verde",
-            "Comino C/N",
-            "Pimentón dulce C/N",
-            "Sal y pimienta C/N"
-        ],
-        directions: [
-            "Rehogar la cebolla con el ají",
-            "Cuando la cebolla se transparente, agregar la carne o soja",
-            "Esperar a que la carne cambie toda de color y condimentar",
-            "Dejar enfriar el relleno",
-            "Rellenar las tapas y hacer el repulgue",
-            "Mandar a horno fuerte 10-15 minutos o hasta que se doren"
-        ],
-        photoPaths: [
-            "./images/empanadas-01.jpg",
-            "./images/empanadas-02.jpg",
-            "./images/empanadas-03.jpg"
-        ]
-    },
-    {
-        name: "Milanesas",
-        description: "Sequitas y crocantes. Para disfrutar al plato o en un sánguche completo.",
-        ingredients: [
-            "500 gr Nalga cortada finamente",
-            "25 gr Provenzal",
-            "Una cucharada de mostaza",
-            "3 huevos",
-            "Pan rallado C/N",
-            "Aceite C/N",
-            "Sal y pimienta C/N"
-        ],
-        directions: [
-            "Limpiar la carne y sacarle el exceso de grasa",
-            "Marinarla con el huevo, sal, pimienta, mostaza y provenzal",
-            "Ir pasándola por pan rallado, presionando bien para que se pegue",
-            "Mandar un rato a la heladera con separadores",
-            "Freir o cocinar al horno con un poco de aceite"
-        ],
-        photoPaths: [
-            "./images/milas-01.jpg",
-            "./images/milas-02.jpg",
-            "./images/milas-03.jpg"
-        ]
-    },
-    {
-        name: "Empanadas",
-        description: "Un clásico, en versiones omnívora y veggie. Para preparar mil y congelar!",
-        ingredients: [
-            "12 tapas",
-            "500 gr Cebolla",
-            "500 gr Carne picada o Soja texturizada hidratada",
-            "1 ají rojo o verde",
-            "Comino C/N",
-            "Pimentón dulce C/N",
-            "Sal y pimienta C/N"
-        ],
-        directions: [
-            "Rehogar la cebolla con el ají",
-            "Cuando la cebolla se transparente, agregar la carne o soja",
-            "Esperar a que la carne cambie toda de color y condimentar",
-            "Dejar enfriar el relleno",
-            "Rellenar las tapas y hacer el repulgue",
-            "Mandar a horno fuerte 10-15 minutos o hasta que se doren"
-        ],
-        photoPaths: [
-            "./images/empanadas-01.jpg",
-            "./images/empanadas-02.jpg",
-            "./images/empanadas-03.jpg"
-        ]
-    },
-    {
-        name: "Milanesas",
-        description: "Sequitas y crocantes. Para disfrutar al plato o en un sánguche completo.",
-        ingredients: [
-            "500 gr Nalga cortada finamente",
-            "25 gr Provenzal",
-            "Una cucharada de mostaza",
-            "3 huevos",
-            "Pan rallado C/N",
-            "Aceite C/N",
-            "Sal y pimienta C/N"
-        ],
-        directions: [
-            "Limpiar la carne y sacarle el exceso de grasa",
-            "Marinarla con el huevo, sal, pimienta, mostaza y provenzal",
-            "Ir pasándola por pan rallado, presionando bien para que se pegue",
-            "Mandar un rato a la heladera con separadores",
-            "Freir o cocinar al horno con un poco de aceite"
-        ],
-        photoPaths: [
-            "./images/milas-01.jpg",
-            "./images/milas-02.jpg",
-            "./images/milas-03.jpg"
+            "/images/milas-01.jpg",
+            "/images/milas-02.jpg",
+            "/images/milas-03.jpg"
         ]
     }
 ]
